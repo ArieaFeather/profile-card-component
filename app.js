2e2e0bcbc8b2b7d31a0a61a7d20cdc7e1dedc317
Leon Smith Jr <creativeinspiration101@gmail.com>
2021-10-10 15:37:18 -0400

    Initial Server Setup

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const app = express();
+
+//DATA PARSING AND VIEW ENGINE
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.set("view engine", 'ejs');
+
+
+
+
+
+app.listen(3000, () => console.log("Press X to Start"))
