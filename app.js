5ce4c887474d3d33f97aff37d8fe9a0fb34c48ed
Leon Smith Jr <creativeinspiration101@gmail.com>
2022-03-23 17:58:08 -0400

    Prepped files for heroku upload

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const app = express();
 
 //DATA PARSING AND VIEW ENGINE
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.set("view engine", 'ejs');
+app.set("view engine", "ejs");
 
 // PAGES
-app.get("/", (req, res) =>{res.render("home");})
-
+app.get("/", (req, res) => {
+    res.render("home");
+});
 
 //SERVER
-app.listen(3000, () => console.log("Press X to Start"))
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, () => {
+    console.log("Quest Started!");
+});
